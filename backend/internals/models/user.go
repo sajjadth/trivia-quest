feda5e9659fabc08a6712379fcb15b1fa592eabe
sajjad <st.655.st@gmail.com>
2023-10-02 19:16:05 +0330

    feat(backend): Add User Model
    This commit introduces the 'User' model with fields for user ID, username, password, email, and creation timestamp. This model will be used for user-related operations.

---
+++ backend/internals/models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	ID        int    `json:"userID"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"createdAt"`
+}
