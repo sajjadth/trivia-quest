bb71947cef2f43d6398a2ae479417c0f30cd1984
sajjad <st.655.st@gmail.com>
2023-10-05 14:24:21 +0330

    feat(db): Update user and verification_attempts tables
    This commit modifies the schema.sql file to update the 'created_at' column in both the 'users' and 'verification_attempts' tables. Now, the 'created_at' field does not allow NULL values to ensure data integrity.

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     username VARCHAR(32) UNIQUE NOT NULL,
     password varchar(128) NOT NULL,
     email VARCHAR(256) UNIQUE NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME NOT NULL
 );
 
 ALTER TABLE users AUTO_INCREMENT = 100000;
@@ -13,7 +13,7 @@ CREATE TABLE verification_attempts (
     user_id INT NOT NULL,
     verification_code VARCHAR(6) NOT NULL,
     verified BOOLEAN DEFAULT false,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    created_at DATETIME NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
