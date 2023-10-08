89a642e03465e613185dea5345c6ec9498e98a40
sajjad <st.655.st@gmail.com>
2023-10-08 12:47:21 +0330

    feat(backend): Add CheckQuestion Endpoint for Answer Validation
    This commit introduces the CheckQuestion endpoint in the handlers package. This endpoint allows for the validation of user answers against correct answers and updates the user's score accordingly. By providing this functionality, the trivia application ensures accurate answer evaluation and enhances user interaction.

--- backend/internals/handlers/questions_handler.go
+++ backend/internals/handlers/questions_handler.go
@@ -26,3 +26,25 @@ func GetQuestions(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"success": true, "results": questions})
 }
+
+func CheckQuestion(c *gin.Context) {
+	var req models.QuestionRequest
+
+	// bind JSON data from the request to the user variable
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": "something went wrong please try again later"})
+		return
+	}
+
+	// get username for accessing to database from extracted token in middleware
+	username, _ := c.Get("username")
+
+	// check user answer with correct answer and making necessary changes in database
+	stat, err := services.CheckQuestion(req.CorrectAnswer, req.UserAnswer, username.(string))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": true, "result": stat})
+}
