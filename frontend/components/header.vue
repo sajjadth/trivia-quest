c3259031196ea39d9f18971b076c07c5e3fdc253
sajjad <st.655.st@gmail.com>
2023-10-09 19:43:36 +0330

    feat(frontend): Add Header Component
    Description:
    This commit introduces a new header component to the frontend application. The header contains the title 'Trivia Quest' along with login and register links. Additionally, adjustments have been made to the default layout to include the header component. This enhancement provides a visual navigation element for users interacting with the application.

---
+++ frontend/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="header">
+    <div id="main">
+      <h3>Trivia Quest</h3>
+      <div id="links">
+        <NuxtLink to="/login">
+          <v-btn variant="text"> login </v-btn>
+        </NuxtLink>
+        <NuxtLink to="/register">
+          <v-btn variant="tonal"> register </v-btn>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+#header
+  width: 100%
+  height: 10%
+  background: transparent
+  display: flex
+  align-items: center
+  justify-content: center
+  padding: 10px
+  #main
+    width: 100%
+    height: 100%
+    border-radius: 22.5px / 22.5px
+    display: flex
+    flex-direction: row
+    align-items: center
+    justify-content: space-between
+    padding: 20px
+  #links
+    width: 200px
+    display: flex
+    align-items: center
+    justify-content: space-between
+</style>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,18 @@
 <template>
   <v-app>
-    <header></header>
-    <NuxtPage />
+    <div id="container">
+      <Header />
+      <NuxtPage />
+    </div>
   </v-app>
 </template>
+
+<style lang="sass">
+#container
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: flex-start
+  width: 100%
+  height: 100svh
+</style>
