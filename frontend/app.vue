34242629eb46c1ef9b9cef4e443d1a142d5e7170
sajjad <st.655.st@gmail.com>
2023-10-22 14:35:11 +0330

    feat(frontend): Implement token verification and username retrieval in app component
    - Utilized the main store to access the 'verifyTokenAndGetUsername' action.
    - Called the action in the mounted lifecycle hook to perform token verification and username retrieval.
    
    This change ensures that token verification and username retrieval are triggered when the app component is mounted.

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,3 +3,16 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script>
+import {useMainStore}from "./store/index"
+export default {
+  setup(){
+    const store = useMainStore()
+    return {store: store}
+  },
+  mounted() {
+    this.store.verifyTokenAndGetUsername()
+  }
+}
+</script>
