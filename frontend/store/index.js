71f3d86d06a5ee72928ab34cf9290589f7f612f4
sajjad <st.655.st@gmail.com>
2023-10-22 14:31:53 +0330

    feat(frontend): Implement token verification and username retrieval
    - Added state variables for 'token', 'username', and 'sessionValid'.
    - Implemented an action 'verifyTokenAndGetUsername' to verify the token and retrieve the username.
    - Utilized the runtime config to access the API base URL.
    - Upon successful verification, set 'sessionValid' to true and redirect to '/app'.
    
    This change enhances the main store by adding functionality to verify tokens and retrieve usernames for authenticated users.

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,6 +1,32 @@
 import { defineStore } from "pinia";
 
 export const useMainStore = defineStore("main", {
-  state: () => ({}),
-  actions: {},
+  state: () => ({
+    token:
+      localStorage.getItem("token") || sessionStorage.getItem("token") || null,
+    username: null,
+    sessionValid: false,
+  }),
+  actions: {
+    verifyTokenAndGetUsername() {
+      if (this.token) {
+        const apiUrl = useRuntimeConfig().public.API_BASE_URL;
+        fetch(`${apiUrl}/auth/verify`, {
+          method: "POST",
+          body: JSON.stringify({ token: this.token }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.success && data.valid) {
+              this.sessionValid = true;
+              this.username = data.username;
+              navigateTo("/app");
+            }
+          })
+          .catch((err) => {
+            console.log("error:", err);
+          });
+      }
+    },
+  },
 });
