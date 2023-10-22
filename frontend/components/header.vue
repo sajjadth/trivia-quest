930829209c341326b0cc31b446271ac8ceff0b35
sajjad <st.655.st@gmail.com>
2023-10-22 14:45:22 +0330

    docs(frontend): Improve header component for user authentication display
    - Organized the header component for better readability and maintainability.
    - Added comments to explain the purpose and functionality of each section.
    - Removed unnecessary spacing and improved code structure.
    - Added TODO comments for future implementation tasks.

--- frontend/components/header.vue
+++ frontend/components/header.vue
@@ -1,21 +1,29 @@
 <template>
+  <!-- Header container -->
   <div id="header">
+    <!-- Main content section -->
     <div id="main">
+      <!-- Site title linking to home -->
       <NuxtLink id="title" to="/">
         <h3>Trivia Quest</h3>
       </NuxtLink>
       <div>
+        <!-- User menu (visible when session is valid) -->
         <v-menu v-if="store.sessionValid" min-width="200px" rounded>
           <template v-slot:activator="{ props }">
+            <!-- User avatar -->
             <v-btn icon v-bind="props">
               <v-avatar size="large">
                 <span class="text-h5">{{ store.username[0] }}</span>
               </v-avatar>
             </v-btn>
           </template>
+
+          <!-- User menu card -->
           <v-card>
             <v-card-text>
               <div class="mx-auto text-center d-flex flex-column">
+                <!-- User avatar and name -->
                 <div
                   id="menu-card-avatar"
                   class="w-100 d-flex flex-column align-center justify-center"
@@ -25,13 +33,21 @@
                   </v-avatar>
                   <span>{{ store.username }}</span>
                 </div>
+                <!-- Divider -->
                 <v-divider class="my-3"></v-divider>
+                <!--
+                  Edit Account button is currently disabled as there is no
+                  functionality for it in both the front and back end.
+                -->
                 <v-btn variant="plain" disabled> Edit Account </v-btn>
+                <!-- Logout button (TODO: Implement functionality) -->
                 <v-btn variant="plain"> LogoUt </v-btn>
               </div>
             </v-card-text>
           </v-card>
         </v-menu>
+
+        <!-- Login and Register links (visible when session is not valid) -->
         <div id="links" v-else>
           <NuxtLink to="/login">
             <v-btn :variant="handleLoginButtonVariant"> login </v-btn>
@@ -83,9 +99,11 @@ export default {
     return { store: store };
   },
   computed: {
+    // Determine login button variant based on current route
     handleLoginButtonVariant() {
       return useRoute().name === "login" ? "tonal" : "plain";
     },
+    // Determine register button variant based on current route
     handleRegisterButtonVariant() {
       return useRoute().name !== "login" ? "tonal" : "plain";
     },
