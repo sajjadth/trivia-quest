173cab38811efac20476975915579dd30d314683
sajjad <st.655.st@gmail.com>
2023-10-22 18:41:50 +0330

    feat(frontend): Add app page for main game functionality
    This commit introduces the pp.vue page responsible for hosting the main game content. The page setup includes access to the main store and triggers a token verification process when leaving the route to ensure a valid session.

---
+++ frontend/pages/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>app</h1>
+</template>
+
+<script>
+import { useMainStore } from "../store/index";
+export default {
+  setup() {
+    const store = useMainStore();
+    return { store: store };
+  },
+  beforeRouteLeave() {
+    this.store.verifyTokenAndGetUsername();
+  },
+};
+</script>
