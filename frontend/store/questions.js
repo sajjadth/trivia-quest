cf3a9bce721ce7151c40022c755a1911b3a512ab
sajjad <st.655.st@gmail.com>
2023-10-27 18:59:01 +0330

    feat(frontend): Integrate Backend API for Question Retrieval
    - Implemented logic to fetch questions from the backend API.
    - Utilized the Pinia store for state management and API interaction.
    
    This change enables the application to communicate with the backend to retrieve questions. The integration is handled within the Pinia store, allowing for seamless data retrieval and management.

--- frontend/store/questions.js
+++ frontend/store/questions.js
@@ -1,13 +1,16 @@
 import { defineStore } from "pinia";
+import { useMainStore } from ".";
 
 export const useQuestionsStore = defineStore("questions", {
   state: () => ({
+    step: 0,
     info: {
       amount: 10,
       category: "Any Category",
       difficulty: "Any Difficulty",
       type: "Any Type",
     },
+    data: [],
     categoryList: ["Any Category"],
     categorysId: [0],
     difficultyList: ["Any Difficulty", "Easy", "Medium", "Hard"],
@@ -20,11 +23,60 @@ export const useQuestionsStore = defineStore("questions", {
     loading: false,
   }),
   actions: {
+    // 'start' action to fetch questions and start the game
+    start() {
+      // Set loading to true
+      this.loading = true;
+
+      // Access the main store
+      const mainStore = useMainStore();
+
+      // Get the API base URL from runtime config
+      const apiUrl = useRuntimeConfig().public.API_BASE_URL;
+
+      // Send a POST request to fetch questions
+      fetch(`${apiUrl}/questions/get`, {
+        method: "POST",
+        headers: {
+          token: mainStore.token, // Include the token from main store in the request headers
+        },
+        body: JSON.stringify({
+          amount: this.info.amount,
+          category:
+            this.categorysId[this.categoryList.indexOf(this.info.category)],
+          difficulty:
+            this.info.difficulty === "Any Difficulty"
+              ? ""
+              : this.info.difficulty.toLocaleLowerCase(), // Convert difficulty to lowercase if not "Any Difficulty"
+          type:
+            this.info.type === "Multiple Choice"
+              ? "multiple"
+              : this.info.type === "True / False"
+              ? "boolean"
+              : "", // Set the question type in the request body
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data.success) {
+            // Open a snackbar with error message if request is not successful
+            this.openSnackbar(data.error, "error");
+          } else {
+            // Set the fetched questions in the data property and increment the step to start game
+            this.data = data.results;
+            this.step++;
+          }
+        })
+        .catch((err) => console.log("error:", err)) // Log any errors to the console
+        .finally(() => (this.loading = false)); // Set loading back to false after request is complete
+    },
+    // 'openSnackbar' action to show a snackbar message
     openSnackbar(message, color) {
       this.snackbar.color = color;
       this.snackbar.message = message;
       this.snackbar.stat = true;
     },
+    // 'closeSnackbar' action to close the snackbar
     closeSnackbar() {
       this.snackbar.stat = false;
       this.snackbar.color = "";
