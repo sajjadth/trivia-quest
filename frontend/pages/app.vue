8aa45350c96f137ec409a7cc3decacbb7333ba63
sajjad <st.655.st@gmail.com>
2023-10-27 19:37:32 +0330

    feat(frontend): Implement Multi-Step Question Selection UI
    - Created a multi-step window component for question selection.
    - Enhanced user experience by breaking the process into manageable steps.
    - Added loading indicators and validation rules for input fields.
    
    This change introduces a structured UI for users to customize their trivia experience. The implementation includes input fields for question quantity, category selection, difficulty level, and question type. Additionally, a 'Start' button initiates the process.

--- frontend/pages/app.vue
+++ frontend/pages/app.vue
@@ -6,57 +6,77 @@
       id="main"
       class="d-flex flex-column align-center justify-center w-100 h-100"
     >
-      <!-- Card containing question options -->
-      <v-card id="select-card" class="rounded-xl">
-        <!-- Card title -->
-        <v-card-title>
-          <h1 class="text-h4 white-space-normal">Question Type Selector</h1>
-        </v-card-title>
-        <!-- Card subtitle -->
-        <v-card-subtitle>
-          <p class="text-subtitle-1">Customize Your Trivia Experience</p>
-        </v-card-subtitle>
-        <!-- Divider line -->
-        <v-divider />
-        <!-- Card content -->
-        <v-card-text>
-          <!-- Input field for number of questions -->
-          <v-text-field
-            type="number"
-            :rules="[
-              (value) => Number(value) >= 1 || 'Should be more than 0',
-              (value) => Number(value) <= 20 || 'Should be less than 21',
-            ]"
-            label="Number of Questions:"
-            variant="outlined"
-            v-model="questions.info.amount"
-          ></v-text-field>
-          <!-- Dropdown for selecting category -->
-          <v-select
-            label="Select Category:"
-            :items="questions.categoryList"
-            variant="outlined"
-            v-model="questions.info.category"
-          ></v-select>
-          <!-- Dropdown for selecting difficulty -->
-          <v-select
-            label="Select Difficulty:"
-            :items="questions.difficultyList"
-            variant="outlined"
-            v-model="questions.info.difficulty"
-          ></v-select>
-          <!-- Dropdown for selecting question type -->
-          <v-select
-            label="Select Type:"
-            :items="questions.typeList"
-            variant="outlined"
-            v-model="questions.info.type"
-          ></v-select>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn block variant="tonal">Start</v-btn>
-        </v-card-actions>
-      </v-card>
+      <!-- Window component to toggle between different steps -->
+      <v-window v-model="questions.step">
+        <!-- Step 0 get questions info-->
+        <v-window-item :value="0">
+          <!-- Card containing question options -->
+          <v-card
+            :loading="questions.loading"
+            id="select-card"
+            class="rounded-xl"
+          >
+            <!-- Card title -->
+            <v-card-title>
+              <h1 class="text-h4 white-space-normal">Question Type Selector</h1>
+            </v-card-title>
+            <!-- Card subtitle -->
+            <v-card-subtitle>
+              <p class="text-subtitle-1">Customize Your Trivia Experience</p>
+            </v-card-subtitle>
+            <!-- Divider line -->
+            <v-divider />
+            <!-- Card content -->
+            <v-card-text>
+              <!-- Input field for number of questions -->
+              <v-text-field
+                type="number"
+                :rules="[
+                  (value) => Number(value) >= 1 || 'Should be more than 0',
+                  (value) => Number(value) <= 20 || 'Should be less than 21',
+                ]"
+                label="Number of Questions:"
+                variant="outlined"
+                v-model="questions.info.amount"
+              ></v-text-field>
+              <!-- Dropdown for selecting category -->
+              <v-select
+                label="Select Category:"
+                :items="questions.categoryList"
+                variant="outlined"
+                v-model="questions.info.category"
+              ></v-select>
+              <!-- Dropdown for selecting difficulty -->
+              <v-select
+                label="Select Difficulty:"
+                :items="questions.difficultyList"
+                variant="outlined"
+                v-model="questions.info.difficulty"
+              ></v-select>
+              <!-- Dropdown for selecting question type -->
+              <v-select
+                label="Select Type:"
+                :items="questions.typeList"
+                variant="outlined"
+                v-model="questions.info.type"
+              ></v-select>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                :disabled="questions.loading"
+                :loading="questions.loading"
+                block
+                variant="tonal"
+                @click="questions.start"
+                >Start</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-window-item>
+
+        <!-- Step 1  main questions(game)-->
+        <v-window-item :value="1"> </v-window-item>
+      </v-window>
     </div>
   </div>
 </template>
