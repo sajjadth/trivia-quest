37393cf757d1a522ddeef6f54e98e4743185a51c
sajjad <st.655.st@gmail.com>
2023-11-04 19:28:03 +0330

    style(frontend): Improve button state handling in password reset request
    - Applied :disabled and :loading attributes to enhance user experience.

--- frontend/pages/password/forgot.vue
+++ frontend/pages/password/forgot.vue
@@ -1,26 +1,22 @@
 <template>
   <!-- Main container with centered content -->
   <v-container class="h-100 d-flex flex-column align-center justify-center">
-
     <!-- Card with maximum width of 500 and centered -->
-    <v-card class="mx-auto">
-
+    <v-card :loading="store.loading" :disabled="store.loading" class="mx-auto">
       <!-- Window component for handling steps -->
       <v-window id="forgot-password-main-window" v-model="store.step">
-
         <!-- Step 0: Request email for password reset -->
         <v-window-item class="w-100 h-100" :value="0">
-
           <!-- Card title with lock icon -->
           <v-card-title
             class="text-h6 text-center font-weight-regular justify-space-between"
           >
             <v-avatar size="100" variant="outlined">
-              <v-icon size="50" icon="mdi-lock"/>
+              <v-icon size="50" icon="mdi-lock" />
             </v-avatar>
           </v-card-title>
 
-           <!-- Card text with trouble logging in message -->
+          <!-- Card text with trouble logging in message -->
           <v-card-text
             id="forgot-password-text"
             class="text-center d-flex flex-column align-center justify-space-between"
@@ -33,7 +29,7 @@
             </p>
           </v-card-text>
 
-           <!-- Email input field -->
+          <!-- Email input field -->
           <v-card-text>
             <v-text-field
               label="Email"
@@ -46,6 +42,7 @@
             <v-btn
               block
               :disabled="!store.info.email"
+              :loading="store.loading"
               color="info"
               variant="elevated"
               @click="store.sendResetPasswordLink"
@@ -97,13 +94,16 @@
 </template>
 
 <script>
-import { useUserStore } from "../store/user.js";
+import { useUserStore } from "../../store/user.js";
+import { useMainStore } from "../../store/index.js";
 
 export default {
   // Get the user store using Pinia and return it as 'store'
   setup() {
     const store = useUserStore();
-    return { store: store };
+    const mainStore = useMainStore();
+
+    return { store: store, mainStore: mainStore };
   },
   unmounted() {
     this.store.$reset();
@@ -118,7 +118,7 @@ export default {
     width: 500px
     padding: 25px
     height: fit-content
-    @media (max-width: $small-screen) 
+    @media (max-width: $small-screen)
       padding: 5px
       width: 100%
     #password-forgot-action
@@ -127,7 +127,7 @@ export default {
     #forgot-password-text
         >*
           padding: 20px 20px 0 20px
-          @media (max-width: $small-screen) 
+          @media (max-width: $small-screen)
             padding: 10px 10px 0 10px
 #done-image
     background: url("../../assets/images/sammy-email-send.png") center no-repeat
