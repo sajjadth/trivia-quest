00c5aa46141a65dfd09736359070ffb2d8c520f8
sajjad <st.655.st@gmail.com>
2023-11-11 19:46:02 +0330

    style(frontend): Improve responsive design for password reset components
    - Adjusted padding in small-screen view for the main container and removed unnecessary padding.
    - Added rounded corners and a subtle border to the cards for a more polished appearance.
    - Applied underlined variant to email input field in the 'Forgot Password' component.
    - Rounded the corners of buttons for better aesthetics.

--- frontend/pages/account/password/forgot.vue
+++ frontend/pages/account/password/forgot.vue
@@ -2,7 +2,12 @@
   <!-- Main container with centered content -->
   <v-container class="h-100 d-flex flex-column align-center justify-center">
     <!-- Card with maximum width of 500 and centered -->
-    <v-card :loading="store.loading" :disabled="store.loading" class="mx-auto">
+    <v-card
+      :loading="store.loading"
+      :disabled="store.loading"
+      class="mx-auto"
+      rounded="xl"
+    >
       <!-- Window component for handling steps -->
       <v-window id="forgot-password-main-window" v-model="store.step">
         <!-- Step 0: Request email for password reset -->
@@ -23,9 +28,8 @@
           >
             <h1 class="text-h5">Trouble logging in?</h1>
             <p class="text-body-1 text-grey-darken-1">
-              Enter your email and we'll send you a
-              <br />
-              link to get back into your account.
+              Enter your email and we'll send you a link to get back into your
+              account.
             </p>
           </v-card-text>
 
@@ -34,7 +38,7 @@
             <v-text-field
               label="Email"
               placeholder="john@google.com"
-              variant="outlined"
+              variant="underlined"
               v-model="store.info.email"
             ></v-text-field>
 
@@ -45,6 +49,7 @@
               :loading="store.loading"
               color="info"
               variant="elevated"
+              rounded="lg"
               @click="store.sendResetPasswordLink"
             >
               Send link
@@ -63,30 +68,33 @@
             id="password-forgot-action"
             class="d-flex flex-column align-center justify-space-between w-100"
           >
-            <NuxtLink to="/register">
-              <v-btn variant="plain">Create new account</v-btn>
-            </NuxtLink>
-            <NuxtLink to="/login">
-              <v-btn variant="plain">Back to login</v-btn>
-            </NuxtLink>
+            <v-btn color="info" variant="plain" rounded="lg" to="/register">
+              Create new account
+            </v-btn>
+            <v-btn color="info" variant="plain" rounded="lg" to="/login">
+              Back to login
+            </v-btn>
           </v-card-actions>
         </v-window-item>
 
         <!-- Step 1: Password Reset Email Sent confirmation -->
-        <v-window-item :value="1">
-          <div id="done-image"></div>
-          <div
-            class="pa-4 text-center d-flex flex-column align-center justify-center"
+        <v-window-item class="w-100 h-100" :value="1">
+          <v-card-item
+            class="d-flex flex-column align-center justify-space-between"
           >
-            <h3 class="text-h6 font-weight-light mb-2">
-              Password Reset Email Sent
-            </h3>
-            <p class="text-caption text-grey">
-              Check your inbox for a password reset email.
-              <br />
-              Follow the instructions to regain access to your account.
-            </p>
-          </div>
+            <div id="done-image"></div>
+            <div
+              class="pa-4 text-center d-flex flex-column align-center justify-center"
+            >
+              <h3 class="text-h6 font-weight-light mb-2">
+                Password Reset Email Sent
+              </h3>
+              <p class="text-caption text-grey">
+                Check your inbox for a password reset email. Follow the
+                instructions to regain access to your account.
+              </p>
+            </div>
+          </v-card-item>
         </v-window-item>
       </v-window>
     </v-card>
@@ -114,12 +122,17 @@ export default {
 <style scoped lang="sass">
 .divider-p-4
     margin-inline: 20px
-#forgot-password-main-window
+    @media(max-width: $small-screen)
+      // padding: 10px
+.v-container
+  @media (max-width: $medium-screen)
+      padding: 0 10px 10px 10px !important
+.v-card
+    padding: 28px 20px
+    border: 1px #333333 solid
     width: 500px
-    padding: 25px
     height: fit-content
     @media (max-width: $small-screen)
-      padding: 5px
       width: 100%
     #password-forgot-action
         >*
