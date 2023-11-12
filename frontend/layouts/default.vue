21afbe47c87a531fe49edce26e7f3fef9d2f0a3a
sajjad <st.655.st@gmail.com>
2023-11-12 11:58:46 +0330

    style(frontend): Update layout styles in default.vue
    - Removed the #container style block.
    - Updated the main container style to use flex properties directly.

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,19 +1,9 @@
 <template>
   <NuxtLoadingIndicator color="#018786" />
   <v-app>
-    <div id="container">
+    <div class="w-100 h-100 d-flex flex-column align-center justify-start">
       <Header />
       <slot />
     </div>
   </v-app>
 </template>
-
-<style lang="sass">
-#container
-  display: flex
-  flex-direction: column
-  align-items: center
-  justify-content: flex-start
-  width: 100%
-  height: 100vh
-</style>
