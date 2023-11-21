6195cfb23c96bfac06ec9790155e12a88ca19ab9
sajjad <st.655.st@gmail.com>
2023-11-21 13:35:54 +0330

    feat(frontend): Enhance question fetching logic
    Improve the 'start' action in the Questions store to fetch questions and start the game. The action now checks the backend status (503 or 200) and handles the question fetching accordingly. When the backend is ready (status 200), it sends a POST request to fetch questions based on user-selected options. If the backend is not ready (status 503), it fetches questions from the local store. This ensures a smooth transition between online and offline modes.

--- frontend/store/questions.js
+++ frontend/store/questions.js
@@ -24,8 +24,55 @@ export const useQuestionsStore = defineStore("questions", {
     loading: false,
   }),
   actions: {
-    // 'start' action to fetch questions and start the game
-    start() {
+    // start quest when status of backend is 200
+    startWithBackend() {
+      // Set loading to true
+      this.loading = true;
+
+      // Access the main store
+      const mainStore = useMainStore();
+
+      // Get the API base URL from runtime config
+      const apiUrl = useRuntimeConfig().public.API_BASE_URL;
+
+      // Send a POST request to fetch questions
+      fetch(`${apiUrl}/questions/get`, {
+        method: "POST",
+        headers: {
+          token: mainStore.token, // Include the token from main store in the request headers
+        },
+        body: JSON.stringify({
+          amount: Number(this.info.amount),
+          category:
+            this.categorysId[this.categoryList.indexOf(this.info.category)],
+          difficulty:
+            this.info.difficulty === "Any Difficulty"
+              ? ""
+              : this.info.difficulty.toLocaleLowerCase(), // Convert difficulty to lowercase if not "Any Difficulty"
+          question_type:
+            this.info.type === "Multiple Choice"
+              ? "multiple"
+              : this.info.type === "True / False"
+              ? "boolean"
+              : "", // Set the question type in the request body
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data.success) {
+            // Open a snackbar with error message if request is not successful
+            mainStore.openSnackbar(data.error, "error");
+          } else {
+            // Set the fetched questions in the data property and increment the step to start game
+            this.data = data.results;
+            this.step++;
+          }
+        })
+        .catch((err) => console.log("error:", err)) // Log any errors to the console
+        .finally(() => (this.loading = false)); // Set loading back to false after request is complete
+    },
+    // start quest when status of backend is 503
+    startWithoutBackend() {
       // Set loading to true
       this.loading = true;
 
@@ -84,6 +131,14 @@ export const useQuestionsStore = defineStore("questions", {
         })
         .catch((err) => console.log("error:", err)) // Log any errors to the console
         .finally(() => (this.loading = false)); // Set loading back to false after request is complete
+    },
+    // 'start' action to fetch questions and start the game
+    start() {
+      // access the main store
+      const mainStore = useMainStore();
+
+      if (mainStore.isBackendReady) this.startWithBackend();
+      else this.startWithoutBackend();
     },
     // 'check' action to check user answer with the correct answer by calling api
     check() {
